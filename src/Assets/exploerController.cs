1038674cc70edaaf45a8c009d7bdbbd23c1778e5
BlakeBonheim <brbonheim@yahoo.com>
2022-01-31 22:50:14 -0500

    unity project

---
+++ src/Assets/exploerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exploerController : MonoBehaviour
+{
+    public Vector2 pos;
+    public float scale;
+    public bool autoLockCursor;
+    public float sensivity = 200.0f;
+    public float currentFloat = 2.0f;
+    public Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void UpdateShader()
+    {
+        float aspect = (float)Screen.width / (float)Screen.height;
+
+        float scaleX = scale;
+        float scaleY = scale;
+        
+        if (aspect > 1f)
+            scaleY /= aspect;
+        else
+            scaleX *= aspect;
+
+        mat.SetVector("_Area", new Vector4(pos.x, pos.y, scaleX, scaleY));
+        
+    }
+
+    private void FixedUpdate()
+    {
+        HandleInputs();
+        UpdateShader();
+
+    }
+
+    // Update is called once per frame
+   private void HandleInputs()
+    {
+
+        //if (Input.GetKey(KeyCode.E))
+        //    scale *= 0.99f;  //zoom in by 1%
+        //if (Input.GetKey(KeyCode.Q))
+        //    scale *= 1.01f; //zoom out by 1%
+
+        if (Input.GetMouseButton(0))
+            scale *= 0.99f;  //zoom in by 1%
+        if (Input.GetMouseButton(1))
+            scale *= 1.01f; //zoom out by 1%
+
+        if (Input.GetKey(KeyCode.W))
+            pos.y += 0.01f * scale;
+        if (Input.GetKey(KeyCode.S))
+            pos.y -= 0.01f * scale;
+        if (Input.GetKey(KeyCode.D))
+            pos.x += 0.01f * scale;
+        if (Input.GetKey(KeyCode.A))
+            pos.x -= 0.01f * scale;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            scale = 4;
+            pos.y = 0;
+            pos.x = 0;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            currentFloat = 2.0f;
+            mat.SetFloat("_Float", currentFloat);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            currentFloat += 0.1f;
+            mat.SetFloat("_Float", currentFloat);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            currentFloat -= 0.1f;
+            mat.SetFloat("_Float", currentFloat);
+        }
+
+
+    }
+}
---
+++ src/Assets/textureChanger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textureChanger : MonoBehaviour
+{
+
+    
+    public float count = 0;
+
+    void Start()
+    {
+         Texture2D texture = new Texture2D(100, 100);
+         GetComponent<Renderer>().material.mainTexture = texture;
+
+
+        for (int y = 0; y < texture.height; y++)
+        {
+            for (int x = 0; x < texture.width; x++)
+            {
+                //Create triangle pattern       Color color = ((x & y) != 0 ? Color.white : Color.grey); 
+                //Color color = new Color((float)255 / texture.height, (float)255 / texture.width, (float)(x + count) / texture.width, 255);
+                    texture.SetPixel(x, y, Color.white);
+                
+            }
+        }
+        texture.Apply();
+
+    }
+
+    void Update()
+    {
+        Texture2D texture = (Texture2D)GetComponent<Renderer>().material.mainTexture;
+
+        //texture.SetPixel(count - 1, (int)Mathf.Abs(Mathf.Sin(count - 1) * texture.height), Color.blue);
+       // for (int x = 0; x < 10; x++)
+        texture.SetPixel((int)count /*+ x*/, 50 + (int)(Mathf.Sin(count) * 50), Color.black);
+      
+        texture.Apply();
+
+        
+        if (count == texture.width)
+            count = 0;
+           
+
+        count += 0.05f;
+       
+    }
+}
